3b457655ac3b5ddc600c2a4ad6d49a02b3b56e46
hisami <koron.take@gmail.com>
2025-11-11 14:35:02 +0900

    graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
+import { ApolloDriver, type ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-	imports: [CatsModule],
+	imports: [
+		CatsModule,
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			playground: true,
+		}),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
