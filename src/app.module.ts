a588b9a26fa288ce785e43157c3e67a0eca5003e
hisami <koron.take@gmail.com>
2025-11-11 10:33:21 +0900

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cat/cats.controller';
+import { CatService } from './cat/cat.service';
 
 @Module({
 	imports: [],
 	controllers: [AppController, CatsController],
-	providers: [AppService],
+	providers: [AppService, CatService],
 })
 export class AppModule {}
--- src/cat/cats.controller.ts
+++ src/cat/cats.controller.ts
@@ -1,29 +1,18 @@
-import {
-	Body,
-	Controller,
-	Delete,
-	Get,
-	Param,
-	Post,
-	Put,
-	Query,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import type { CatService } from './cat.service';
 import type { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
+	constructor(private readonly catService: CatService) {}
+
 	@Post()
 	create(@Body() createCatDto: CreateCatDto) {
-		return 'This action adds a new cat';
+		this.catService.create(createCatDto);
 	}
 
-	@Get(':id')
-	findOne(@Param('id') id: string) {
-		return `This action returns a #${id} cat`;
-	}
-
-	@Delete(':id')
-	remove(@Param('id') id: string) {
-		return `This action removes a #${id} cat`;
+	@Get()
+	findAll() {
+		return this.catService.findAll();
 	}
 }
