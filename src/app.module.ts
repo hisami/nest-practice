5aaa5530fd45bf30ca33e6c17e0c11f422536c01
hisami <koron.take@gmail.com>
2025-11-11 11:17:16 +0900

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cat/cats.controller';
-import { CatService } from './cat/cat.service';
+import { CatModule } from './cat/cat.module';
 
 @Module({
-	imports: [],
-	controllers: [AppController, CatsController],
-	providers: [AppService, CatService],
+	imports: [CatModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
